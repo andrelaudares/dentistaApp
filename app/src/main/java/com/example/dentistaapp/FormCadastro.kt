de447b225ba3c5105e823bd2a67997f6292755ef
André Laudares Soares <83609153+andrelaudares@users.noreply.github.com>
2023-04-24 23:06:15 -0300

    firebase auth tentativa1

--- app/src/main/java/com/example/dentistaapp/FormCadastro.kt
+++ app/src/main/java/com/example/dentistaapp/FormCadastro.kt
@@ -1,16 +1,25 @@
 package com.example.dentistaapp
 
+import android.content.ContentValues
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.dentistaapp.databinding.ActivityFormCadastroBinding
+import com.example.dentistaapp.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
+
+private  lateinit var binding: ActivityMainBinding
+private lateinit var auth: FirebaseAuth
+private fun updateUI(user: FirebaseUser?) {}
 
 class FormCadastro : AppCompatActivity() {
 
-    private  lateinit var binding: ActivityFormCadastroBinding
+    private lateinit var binding: ActivityFormCadastroBinding
     private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +27,7 @@ class FormCadastro : AppCompatActivity() {
         binding = ActivityFormCadastroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCriarCad.setOnClickListener{view ->
+        binding.btnCriarCad.setOnClickListener { view ->
             val email = binding.etEmailCad.text.toString()
             val senha = binding.etSenhaCad.text.toString()
             val endereco1 = binding.etEndereco1Cad.text.toString()
@@ -29,15 +38,15 @@ class FormCadastro : AppCompatActivity() {
             val telefone = binding.etTelefoneCad.text.toString()
             val curriculo = binding.etCurriculoCad.text.toString()
 
-            if(email.isEmpty() || senha.isEmpty() || endereco1.isEmpty() || nome.isEmpty() || cpf.isEmpty() || curriculo.isEmpty()){
-                val snackbar = Snackbar.make(view, "Preencha todos os campos!", Snackbar.LENGTH_SHORT)
+            if (email.isEmpty() || senha.isEmpty() || endereco1.isEmpty() || nome.isEmpty() || cpf.isEmpty() || curriculo.isEmpty()) {
+                val snackbar =
+                    Snackbar.make(view, "Preencha todos os campos!", Snackbar.LENGTH_SHORT)
                 snackbar.setBackgroundTint(Color.RED)
                 snackbar.show()
 
 
-
-            }else{
-                binding.btnCriarCad.setOnClickListener{
+            } else {
+                binding.btnCriarCad.setOnClickListener {
 
                     val usuarioMap = hashMapOf(
 
@@ -52,16 +61,34 @@ class FormCadastro : AppCompatActivity() {
 
                     )
                     db.collection("Usuários").document(email)
-                        .set(usuarioMap).addOnCompleteListener{
+                        .set(usuarioMap).addOnCompleteListener {
                             Log.d("db", "sucesso ao salvar o usuário!")
 
-                        }.addOnFailureListener{
+                        }.addOnFailureListener {
                         }
 
                 }
+                auth.createUserWithEmailAndPassword(email, senha)
+                    .addOnCompleteListener(this) { task ->
+
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(ContentValues.TAG, "createUserWithEmail:success")
+                            val user = auth.currentUser
+                            updateUI(user)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(ContentValues.TAG, "createUserWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            updateUI(null)
+                        }
+                    }
             }
+
+
         }
-
-
     }
 }
--- app/src/main/java/com/example/dentistaapp/MainActivity.kt
+++ app/src/main/java/com/example/dentistaapp/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.dentistaapp
 
+import android.content.ContentValues.TAG
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
-import com.google.firebase.ktx.Firebase
+import com.example.dentistaapp.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private fun updateUI(user: FirebaseUser?) {}
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         val buttonCadastro = findViewById<AppCompatButton>(R.id.btnIrCad)
 
@@ -19,5 +34,36 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,FormCadastro::class.java)
             startActivity(intent)
         }
+
+        binding.btnLogin.setOnClickListener{
+            val email = binding.etEmail.text.toString()
+            val senha = binding.etSenha.text.toString()
+
+            if(email.isEmpty() || senha.isEmpty()){
+                val snackbar = Snackbar.make(it, "Preencha todos os campos", Snackbar.LENGTH_SHORT)
+                snackbar.setBackgroundTint((Color.RED))
+                snackbar.show()
+
+            }else{
+                auth.signInWithEmailAndPassword(email, senha)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Log.d(TAG, "signInWithEmail:success")
+                            val user = auth.currentUser
+                            updateUI(user)
+                        } else {
+
+                            Log.w(TAG, "signInWithEmail:failure", task.exception)
+                            Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                            updateUI(null)
+                        }
+                    }
+                }
+
+
+            }
+        }
+
+
     }
-}
